bd1c38ede4360019b42a46e478bac6d867b2c81b
cff <chenfengfu0516@foxmail.com>
2018-02-07 16:27:42 +0800

    CFF: jdbc

--- javaBase/src/main/java/com/jdbc/DBUtils.java
+++ javaBase/src/main/java/com/jdbc/DBUtils.java
@@ -1,9 +1,7 @@
 package com.jdbc;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.junit.Test;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -14,29 +12,25 @@ public class DBUtils {
     private static String url = null;
     private static String user = null;
     private static String password = null;
-    private static BasicDataSource ds = null;
+    private static BasicDataSource ds = null; // 数据源
+
     static {
-        //读取程序外的.properties文件
-        //需要.properties文件的包路径
         Properties props = new Properties();
         try {
-         String path = "./src/main/java/com/jdbc/db.properties";
-//            props.load(DBUtils.class.getClassLoader().getResourceAsStream(path));
+            String path = "./src/main/java/com/jdbc/db.properties";
+// props.load(DBUtils.class.getClassLoader().getResourceAsStream("./com/jdbc/db.properties")); // 默认是从ClassPath根下获取文件
+// props.load(DBUtils.class.getResourceAsStream("./db.properties")); // .class文件为根目录
             props.load(new FileInputStream(path));
-            //返回String value=properties对象.getProperty(“key”)
             driver = props.getProperty("driver");
             url = props.getProperty("url");
             user = props.getProperty("user");
             password = props.getProperty("password");
             ds = new BasicDataSource();
-            //ds中已经有了几个创建好的连接
             ds.setDriverClassName(driver);
             ds.setUrl(url);
             ds.setUsername(user);
             ds.setPassword(password);
-            ds.setInitialSize(
-                    Integer.parseInt(props.getProperty("initialSize")
-                    )
+            ds.setInitialSize(Integer.parseInt(props.getProperty("initialSize"))
             );
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -57,19 +51,5 @@ public class DBUtils {
         }
     }
 
-    @Test
-    public void  test() throws Exception{
-//        String path = "./src/main/java/com/jdbc/db.properties";
-        String path = ".";
-
-        File file = new File(path);
-//        System.out.println(file.listFiles()[0].getName());
-        System.out.println(file.exists());
-//        Properties properties = new Properties();
-//            String path = "jdbc/db.properties";
-//            props.load(DBUtils.class.getClassLoader().getResourceAsStream(path));
-//        properties.load(new FileInputStream(path));
-
-    }
 
 }
