d9fbb0cb69d85eb7cbd4345a0f796e765c3f7afd
fengfuChen <979566434@qq.com>
2018-02-07 20:16:33 +0800

    CFF:

--- javaBase/src/main/java/com/jdbc/UserDAO.java
+++ javaBase/src/main/java/com/jdbc/UserDAO.java
@@ -16,11 +16,24 @@ public class UserDAO {
         ResultSet rs = null;
         try {
             conn = DBUtils.getConnection();
-            //Step2：定义SQL语句
             stmt = conn.createStatement();
-            //Step3：执行SQL语句
             String sql = "SELECT id, name, age FROM sys_user LIMIT 0, 1";
+
+            // sql = SELECT id, name, age FROM sys_user
+            //  注入攻击 sql = sql + "where name =" + name + "and pwd =" + pwd  当传递参数为 "jack", "or 1=1" 时可获取全部用户信息
             rs = stmt.executeQuery(sql);
+
+            /*
+            String sql = "SELECT id, name, age FROM sys_user LIMIT ?, ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setObject(0, 0)
+            pstmt.setObject(0, 1)
+            //PreparedStatement  可防止注入式攻击
+            SELECT id, name, age FROM sys_user  where name = ? ,  当传递参数为 "jack", "or 1=1" 时
+             SELECT id, name, age FROM sys_user  where name = 'jack' and password = '\' or \'1\'=\'1', 注入攻击失败
+            rs= pstmt.executeQuery();
+            */
+
             //Step4：处理结果集
             while (rs.next()) {
                 //列类型、get的类型、变量类型一致
