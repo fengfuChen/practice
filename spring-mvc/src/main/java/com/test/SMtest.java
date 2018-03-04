062ed09ec1b9eea8876ea59a9974eafb1630c395
fengfuChen <979566434@qq.com>
2018-03-05 00:56:53 +0800

    CFF: spring - mybatis

---
+++ spring-mvc/src/main/java/com/test/SMtest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * Created by chenfengfu on 2018/3/5.
+ */
+public class SMtest {
+
+
+    @Test
+    public void aa(){
+//        String resource = "config/mybatis-config.xml";
+//        InputStream inputStream = SMtest.class.getClassLoader().getResourceAsStream(resource);
+//        //构建sqlSession的工厂
+//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//        //根据 sqlSessionFactory 产生 session
+//        SqlSession session = sessionFactory.openSession();
+//        userMapper.addUser(new User());
+//        session.commit();
+//        session.close();
+    }
+}
